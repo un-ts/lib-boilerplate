3d0c8d1802f74662c34812e2e89b7a8008cadcd0
JounQin <admin@1stg.me>
2025-04-07 00:47:41 +0800

    chore: use `exports` for dev and `publishConfig` for prod

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'lib-boilerplate': new URL('src/index.ts', import.meta.url).pathname,
-    },
-  },
   test: {
     globals: true,
     coverage: {
