5db37125384680cbaa18c1145f11c021d0b71d52
JounQin <admin@1stg.me>
2025-05-20 13:36:40 +0800

    chore: specific `--tsconfig` to workaround

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'lib-boilerplate': new URL('src/index.ts', import.meta.url).pathname,
+    },
+  },
   test: {
     globals: true,
     coverage: {
